27532bf650635761cc797830bf9e287225b89d95
Flavien DELANGLE <flaviendelangle@gmail.com>
2018-11-12 11:45:57 +0100

    Add Gitignore

---
+++ components/Tag/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+import { generateColorFromSeed } from '../../utils/colors'
+
+const Tag = styled.div`
+  background: ${({ colorSeed }) => generateColorFromSeed(colorSeed)};
+
+  padding: 2px 8px;
+  display: inline;
+  border-radius: 2px;
+
+  color: white;
+  font-weight: 600;
+  font-size: 14px;
+`
+
+Tag.propTypes = {
+  colorSeed: PropTypes.string.isRequired
+}
+
+export default Tag
---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as Tag } from './components/Tag'
