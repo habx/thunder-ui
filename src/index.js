54ddfd5366b728605d856b848e523a90df0b7762
Flavien DELANGLE <flaviendelangle@gmail.com>
2018-11-14 15:12:37 +0100

    Add color theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as Tag } from './components/Tag/index'
+
+export { default as theme } from './theme'
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const COLOR_MAASTRICHT_BLUE = '#061a3c'
+export const COLOR_PAYNES_GREY = '#52627a'
+export const COLOR_TRUE_BLUE = '#0071ce'
+export const COLOR_BRIGHT_CERUALEAN = '#1dcad3'
+export const COLOR_INTERNATIONAL_ORANGE = '#ff5100'
+export const COLOR_USC_GOLD = '#ffce00'
+export const COLOR_POPSTAR = '#be3b5d'
+
+export default {
+  colors: {
+    maastrichtBlue: COLOR_MAASTRICHT_BLUE,
+    paynesGrey: COLOR_PAYNES_GREY,
+    trueBlue: COLOR_TRUE_BLUE,
+    brightCerualean: COLOR_BRIGHT_CERUALEAN,
+    internationalOrange: COLOR_INTERNATIONAL_ORANGE,
+    uscGold: COLOR_USC_GOLD,
+    popstar: COLOR_POPSTAR
+  }
+}
