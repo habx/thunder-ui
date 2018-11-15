8953ceeac14a5c164a22af1ff4251e2de37afd5c
Flavien DELANGLE <flaviendelangle@gmail.com>
2018-11-15 16:41:21 +0100

    Fix Babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,3 @@
-const presets = [
-  "@babel/preset-react",
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
-      },
-      useBuiltIns: "usage",
-    },
-  ],
-];
+const presets = ["@babel/preset-react"];
 
 module.exports = { presets };
