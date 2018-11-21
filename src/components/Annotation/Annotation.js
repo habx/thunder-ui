bab33c2d1746582875480b3bf85ac6c3ade86e27
Flavien DELANGLE <flaviendelangle@gmail.com>
2018-11-21 10:11:13 +0100

    Fix lint

--- src/components/Annotation/Annotation.js
+++ src/components/Annotation/Annotation.js
@@ -3,12 +3,12 @@ import styled from 'styled-components'
 import { fontSizes, colors } from '../../theme'
 
 const prepareProps = props => ({
-  color: props.color || colors.paynesGrey
+  color: props.color || colors.paynesGrey,
 })
 
 const Annotation = styled.span.attrs(prepareProps)`
   font-size: ${fontSizes.tiny};
-  color: ${({ color }) => color }
+  color: ${({ color }) => color}
 `
 
 export default Annotation
