706eb6d3e430b587fc1ad52c197e9012a5f036d7
Flavien DELANGLE <flaviendelangle@gmail.com>
2018-11-29 15:36:40 +0100

    APP-2481 New Component : Card (+ refacto Titles)

---
+++ src/components/Card/Card.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+import Title from '../Title'
+import { borderRadius, colors, shadows } from '../../theme'
+
+const CardContainer = styled.div`
+  box-shadow: ${shadows.light};
+  border-radius: ${borderRadius.narrow};
+  
+  
+  ${({ onClick }) => onClick && css`
+    cursor: pointer;
+    transition: box-shadow 150ms ease-in-out;
+    
+    &:hover {
+      box-shadow: ${shadows.strong};
+    }
+  `};
+`
+
+const TitleContainer = styled.div`
+  padding: 24px 24px 8px 24px;
+`
+
+const SubtitleContainer = styled.div`
+  padding: 0 24px 8px 24px;
+`
+
+const Card = ({ title, subtitle, children, ...props }) => (
+  <CardContainer {...props}>
+    {
+      title &&
+      <TitleContainer>
+        <Title size={3}>{ title }</Title>
+      </TitleContainer>
+    }
+    {
+      subtitle &&
+      <SubtitleContainer>
+        <Title size={4} color={colors.paynesGrey}>{ subtitle }</Title>
+      </SubtitleContainer>
+    }
+    { children }
+  </CardContainer>
+)
+
+export default Card
--- src/theme.js
+++ src/theme.js
@@ -41,6 +41,7 @@ export const borderRadius = {
 }
 
 export const shadows = {
+  strong: '0 4px 12px 0 rgba(80, 79, 79, 0.24)',
   light: '0 2px 6px 0 rgba(6, 26, 60, 0.16)',
 }
 
