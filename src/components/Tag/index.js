4d70e8130b29f0bfd62ebd85d84fca0e42843d64
Jean Dessane <jean@habx.fr>
2018-11-30 12:52:55 +0100

    tags

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -1,21 +1,26 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
+import colorUtil from 'color'
+
 import { generateColorFromSeed } from '../../internal/colors'
+import { colors } from '../../theme'
 
 const Tag = styled.div`
   background-color: ${({ colorSeed, color }) => color || generateColorFromSeed(colorSeed)};
 
-  padding: 2px 8px;
+  padding: 4px 16px;
   display: inline;
-  border-radius: 2px;
+  border-radius: 12px;
+  
+  font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
 
-  color: white;
-  font-weight: 600;
+  color: ${({ colorSeed }) => (colorSeed ? 'white' : colorUtil(colors.paynesGrey).fade(0.28).toString())};
   font-size: 14px;
 `
 
 Tag.propTypes = {
+  bold: PropTypes.bool,
   colorSeed: PropTypes.string,
   color: PropTypes.string,
 }
