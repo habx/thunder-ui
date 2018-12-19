26049bc55ff9c01943724d8066f9922fef48a621
Jean Dessane <jean@habx.fr>
2018-12-19 15:59:57 +0100

    remove infos

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,11 +1,9 @@
 import { configure, addDecorator } from '@storybook/react'
 import centered from '@storybook/addon-centered'
-import { withInfo } from '@storybook/addon-info'
 import fontDecorator from './fontDecorator'
 
 addDecorator(centered)
 addDecorator(fontDecorator)
-addDecorator(withInfo)
 
 const req = process.env.NODE_ENV === 'test' ?
   require('./requireContext')('../src/components', true, /\.stories\.(tsx)$/) :
