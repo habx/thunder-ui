4ead30f6e40b227d2b6e26d2ef9a2f33e57c370c
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-01-08 15:56:46 +0100

    APP-2969 New Component : Modal

---
+++ src/components/Modal/Modal.interface.ts
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+import CardProps from '../Card/Card.interface'
+
+export default interface ModalProps extends CardProps {
+  open: boolean
+  onClose: (e: React.FormEvent<HTMLInputElement>) => void
+  title?: string
+  headerPosition?: never
+}
--- src/components/Thunder/Thunder/index.tsx
+++ src/components/Thunder/Thunder/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { withTheme } from 'styled-components'
 
-import Thunder from './Thunder'
+import InnerThunder from './Thunder'
 import ThunderProps from './Thunder.interface'
 
-const EndhancedThunder = withTheme(Thunder)
+const EndhancedThunder = withTheme(InnerThunder)
 
-const Wrapper: React.StatelessComponent<ThunderProps> = ({ theme, ...props }) => (
+const Thunder: React.StatelessComponent<ThunderProps> = ({ theme, ...props }) => (
   <EndhancedThunder customTheme={theme} {...props} />
 )
 
-export default Wrapper
+export default Thunder
