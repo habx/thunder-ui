1b0610ab96df51b4b02bc736e621aaf20a26c03a
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-01-13 15:09:12 +0100

    APP-3038 Use Inter UI font instead of Euclid Circular

--- .storybook/fontDecorator.tsx
+++ .storybook/fontDecorator.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const Fonts = createGlobalStyle`
   * {
-    font-family: EuclidCircularB;
+    font-family: Inter UI;
   }
 
   @font-face {
@@ -39,41 +39,6 @@ const Fonts = createGlobalStyle`
     src: url("fonts/Habx/Habx-Regular.eot") format("eot"),
       url("fonts/Habx/Habx-Regular.woff2") format("woff2"),
       url("fonts/Habx/Habx-Regular.woff") format("woff");
-  }
-
-  @font-face {
-    font-family: "EuclidCircularB";
-    font-style: normal;
-    font-weight: bold;
-    src: url("fonts/EuclidCircularB/EuclidCircularB-Bold.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "EuclidCircularB";
-    font-style: normal;
-    font-weight: '300';
-    src: url("fonts/EuclidCircularB/EuclidCircularB-Light.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "EuclidCircularB";
-    font-style: normal;
-    font-weight: '500';
-    src: url("fonts/EuclidCircularB/EuclidCircularB-Medium.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "EuclidCircularB";
-    font-style: normal;
-    font-weight: '600';
-    src: url("fonts/EuclidCircularB/EuclidCircularB-Semibold.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "EuclidCircularB";
-    font-style: normal;
-    font-weight: normal;
-    src: url("fonts/EuclidCircularB/EuclidCircularB.otf") format("opentype");
   }
 `
 
