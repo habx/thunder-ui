4d7b9577ff0e5528b39f6a5a2a5d3221c22081f9
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-01-23 13:20:33 +0100

    Add stories

---
+++ src/confirm/confirm.stories.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import confirm from './index'
+
+import HabxProvider from '../HabxProvider'
+import Button from '../Button'
+
+const habxProvider = storyFn => (
+  <HabxProvider>
+    { storyFn() }
+  </HabxProvider>
+)
+
+storiesOf('Events/confirm', module)
+  .addDecorator(habxProvider)
+  .add('basic', () => (
+    <Button
+      onClick={async () => {
+        const response = await confirm('Voulez-vous continuer')
+        action('Confirm Modal response')(response)
+      }}>
+      Trigger event
+    </Button>
+  ))
+  .add('with custom button texts', () => (
+    <Button
+      onClick={async () => {
+        const response = await confirm('Voulez-vous continuer', { confirmText: 'Oui', cancelText: 'Non' })
+        action('Confirm Modal response')(response)
+      }}>
+      Trigger event
+    </Button>
+  ))
---
+++ src/notify/notify.stories.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import notify from './index'
+
+import HabxProvider from '../HabxProvider'
+import Button from '../Button'
+
+const habxProvider = storyFn => (
+  <HabxProvider>
+    { storyFn() }
+  </HabxProvider>
+)
+
+storiesOf('Events/notify', module)
+  .addDecorator(habxProvider)
+  .add('basic', () => (
+    <Button onClick={() => notify('This is a notification')}>
+      Trigger event
+    </Button>
+  ))
+  .add('error', () => (
+    <Button onClick={() => notify('This is a notification', { type: 'error' })}>
+      Trigger event
+    </Button>
+  ))
+  .add('warning', () => (
+    <Button onClick={() => notify('This is a notification', { type: 'warning' })}>
+      Trigger event
+    </Button>
+  ))
+
+  .add('with custom duration', () => (
+    <Button onClick={() => notify('This is a notification', { duration: 1000 })}>
+      Trigger event
+    </Button>
+  ))
+  .add('without auto close', () => (
+    <Button onClick={() => notify('This is a notification', { duration: 0 })}>
+      Trigger event
+    </Button>
+  ))
