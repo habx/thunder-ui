bfd0315c2a1a54b8a2a6afd51b96512e148094d5
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-01-28 18:26:16 +0100

    Clean stories

--- src/Select/Select.stories.tsx
+++ src/Select/Select.stories.tsx
@@ -5,10 +5,7 @@ import { withState } from 'recompose'
 
 import FontIcon from '../FontIcon'
 import Select from './index'
-import { shortData } from './Select.data'
-
-const DEFAULT_VALUE_FULL = shortData[2]
-const DEFAULT_VALUE_SIMPLE = shortData[2].value
+import {longData, shortData} from './Select.data'
 
 const SelectWithState = ({ value = null, ...props }) => {
   const Component = withState('value', 'onChange', value)(newProps => (
@@ -31,7 +28,7 @@ storiesOf('Inputs/Select', module)
     <SelectWithState />
   ))
   .add('filterable', () => (
-    <SelectWithState filterable />
+    <SelectWithState filterable options={longData} />
   ))
   .add('disabled', () => (
     <SelectWithState disabled />
@@ -42,24 +39,12 @@ storiesOf('Inputs/Select', module)
   .add('with description', () => (
     <SelectWithState description='Please pick an option' annotation='The one you prefer' />
   ))
-  .add('single item with value (label, value mode)', () => (
-    <SelectWithState value={DEFAULT_VALUE_FULL} />
+  .add('with icon', () => (
+    <SelectWithState icon={<FontIcon icon='camera_enhance' />} />
   ))
-  .add('single item with value (simple value mode)', () => (
-    <SelectWithState value={DEFAULT_VALUE_SIMPLE} />
-  ))
-  .add('single item without reset', () => (
+  .add('without reset', () => (
     <SelectWithState canReset={false} />
   ))
   .add('multi items', () => (
     <SelectWithState isMulti />
-  ))
-  .add('multi items with value (label, value mode)', () => (
-    <SelectWithState isMulti value={[DEFAULT_VALUE_FULL]} />
-  ))
-  .add('multi items with value (simple value mode)', () => (
-    <SelectWithState isMulti value={[DEFAULT_VALUE_SIMPLE]} />
-  ))
-  .add('multi items with icon', () => (
-    <SelectWithState isMulti icon={<FontIcon icon='camera_enhance' />} />
   ))
