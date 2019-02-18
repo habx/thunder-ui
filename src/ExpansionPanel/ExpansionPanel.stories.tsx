d77192aa2bd8d5888b73e5b88a583619d566f260
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-02-18 15:14:52 +0100

    New story

--- src/ExpansionPanel/ExpansionPanel.stories.tsx
+++ src/ExpansionPanel/ExpansionPanel.stories.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 
+import FontIcon from '../FontIcon'
 import ExpansionPanel from './index'
 import ExpansionPanelItem from '../ExpansionPanelItem'
 import { regularData } from './ExpansionPanel.data'
@@ -21,6 +22,26 @@ storiesOf('Layouts/ExpansionPanel', module)
   .add('basic uncontrolled', () => (
     <ExpansionPanel>
       <ExpansionPanelItem title='First item'>
+        { regularData }
+      </ExpansionPanelItem>
+      <ExpansionPanelItem title='Second item'>
+        { regularData }
+      </ExpansionPanelItem>
+      <ExpansionPanelItem title='Third item'>
+        { regularData }
+      </ExpansionPanelItem>
+      <ExpansionPanelItem title='Fourth item'>
+        { regularData }
+      </ExpansionPanelItem>
+    </ExpansionPanel>
+  ))
+  .add('with custom icons', () => (
+    <ExpansionPanel>
+      <ExpansionPanelItem
+        title='First item'
+        expandIcon={<FontIcon icon='add' />}
+        collapseIcon={<FontIcon icon='remove' />}
+      >
         { regularData }
       </ExpansionPanelItem>
       <ExpansionPanelItem title='Second item'>
