14df4f757ea2778e8f5936f1fd6dedddbbea319f
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-02-19 14:46:11 +0100

    APP-3555 Add error prop to Card

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -7,33 +7,32 @@ import Title from '../Title'
 import CardProps, { CardInnerProps } from './Card.interface'
 import { CardContainer, TitleContainer, SubtitleContainer, TitleCount } from './Card.style'
 
-const BaseCard: React.StatelessComponent<CardInnerProps> = ({ headerPosition, action, title, titleCount, subtitle, children, ...props }) => (
-  <React.Fragment>
-    {
-      title && headerPosition === 'outside' &&
-      <TitleContainer>
-        <Title size={3}>{ title }{titleCount ? <TitleCount>({ titleCount })</TitleCount> : null}</Title>
-        { action }
-      </TitleContainer>
-    }
-    <CardContainer {...props}>
-      {
-        title && headerPosition === 'inside' &&
-        <TitleContainer>
-          <Title size={3}>{ title }{(titleCount || titleCount === 0) ? <TitleCount>({ titleCount })</TitleCount> : null}</Title>
-          { action }
-        </TitleContainer>
-      }
-      {
-        subtitle &&
-        <SubtitleContainer>
-          <Title size={4} color={getMainColor(props, { themeKey: 'neutral' })}>{ subtitle }</Title>
-        </SubtitleContainer>
-      }
-      { children }
-    </CardContainer>
-  </React.Fragment>
-)
+const BaseCard: React.StatelessComponent<CardInnerProps> = ({ headerPosition, action, title, titleCount, subtitle, children, ...props }) => {
+  const titleElement = title && (
+    <TitleContainer>
+      <Title size={3} color={getMainColor(props, { themeKey: 'neutralDark' })}>
+        { title }{(titleCount || titleCount === 0) ? <TitleCount>({ titleCount })</TitleCount> : null}
+      </Title>
+      { action }
+    </TitleContainer>
+  )
+
+  return (
+    <React.Fragment>
+      { headerPosition === 'outside' && titleElement }
+      <CardContainer {...props}>
+        { headerPosition === 'inside' && titleElement }
+        {
+          subtitle &&
+          <SubtitleContainer>
+            <Title size={4} color={getMainColor(props, { themeKey: 'neutral' })}>{ subtitle }</Title>
+          </SubtitleContainer>
+        }
+        { children }
+      </CardContainer>
+    </React.Fragment>
+  )
+}
 
 BaseCard.defaultProps = {
   headerPosition: 'inside',
