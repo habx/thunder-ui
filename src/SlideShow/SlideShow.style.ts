45b4f364206b0539a76c07b445c167bdcc090a87
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-02-26 12:11:10 +0100

    APP-3461 Mode the CRM's SlideShow into the lib UI

---
+++ src/SlideShow/SlideShow.style.ts
@@ -0,0 +1,60 @@
+import styled, { css } from 'styled-components'
+
+export const SlideshowContainer = styled.div`
+  width: 100%;
+  position: relative;
+  overflow-x: hidden;
+`
+
+export const SlideshowContent = styled.div`
+  width: 100%;
+  display: flex;
+  transition: transform ease-in-out ${({ transitionDuration }) => transitionDuration}ms;
+  transform: translateX(0);
+  ${({ length }) => css`
+    ${new Array(length)
+    .fill(0)
+    .map(
+      (_, i) => `
+      &[data-index="${i}"] {
+        transform: translateX(-${i * 100}%);
+      }
+    `
+    )
+    .join('')}
+  }`}
+`
+
+export const SlideshowElement = styled.div`
+  width: 100%;
+  flex: 0 0 100%;
+  margin-bottom: auto;
+  margin-top: auto;
+`
+
+export const NavigationDotsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 32px;
+  margin-bottom: 16px;
+
+  &:hover {
+    cursor: ${({ canNavigate }) => (canNavigate ? 'pointer' : 'auto')};
+  }
+`
+
+export const Dot = styled.div`
+  display: inline-block;
+  margin: 0 1.5px;
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.3);
+  vertical-align: middle;
+  transition: width 300ms ease-in-out;
+
+  &[data-active='true'] {
+    width: 16px;
+    background-color: ${({ color }) => color};
+  }
+`
