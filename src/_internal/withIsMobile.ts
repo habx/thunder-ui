7a241bd24d1af323f348e88d88eb99742242bc19
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-02-27 16:09:55 +0100

    Remove unused HOCs

--- src/_internal/withIsMobile.ts
+++
@@ -1,9 +0,0 @@
-import withSizes from './withSizes'
-
-const mapSizesToProps = ({ width }) => ({
-  isMobile: width <= 600
-})
-
-const withIsMobile = Component => withSizes(mapSizesToProps)(Component)
-
-export default withIsMobile
