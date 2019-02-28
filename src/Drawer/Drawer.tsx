c864209e37f5e4265916845e3ca3e2ff781b4a4f
Jean Dessane <jean9696@users.noreply.github.com>
2019-02-28 16:29:28 +0100

    Bubling event instead of capturing (#185)

--- src/Drawer/Drawer.tsx
+++ src/Drawer/Drawer.tsx
@@ -36,7 +36,7 @@ class Drawer extends PureComponent<DrawerProps> {
 
   componentDidMount () {
     window.addEventListener('keydown', this.handleKeyDown)
-    window.addEventListener('click', this.handleClick, true)
+    window.addEventListener('click', this.handleClick)
 
     if (this.props.open) {
       this.animateOpening()
