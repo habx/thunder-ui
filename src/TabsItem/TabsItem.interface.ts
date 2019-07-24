38853cce435fcb250ec87bf4f61802f5951e0174
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-07-24 10:38:32 +0200

    APP-5931 Migrate client-projects to TS

--- src/TabsItem/TabsItem.interface.ts
+++ src/TabsItem/TabsItem.interface.ts
@@ -1,9 +1,12 @@
 import * as React from 'react'
 
+import { Except } from '../_internal/types'
+
 export default interface TabsItemProps
-  extends React.LiHTMLAttributes<HTMLLIElement> {
+  extends Except<React.LiHTMLAttributes<HTMLLIElement>, 'is'> {
   activeColor?: string
   hoverColor?: string
   closed?: boolean
   active?: boolean
+  is?: React.ComponentType<any>
 }
