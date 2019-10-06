eaf4f8d66af4f6d65bffd9751a82b19a43f4c77f
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-10-06 16:11:31 +0200

    Upgrade CI

--- bili.config.ts
+++ bili.config.ts
@@ -1,19 +1,6 @@
 import { Config } from 'bili'
 
 const config: Config = {
-  plugins: {
-    babel: false,
-    typescript2: {
-      tsconfigOverride: {
-        compilerOptions: {
-          declaration: false,
-          declarationMap: false,
-        },
-      },
-    },
-  },
-
-  // Let's take this opportunity to move the CLI flags here as well
   input: 'src/index.ts',
   output: {
     format: ['cjs', 'esm'],
